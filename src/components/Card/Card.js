bb22f41dd99c829c96918b8919431e046b7f7654
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-04 21:57:01 +0100

    add personal data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,19 +3,23 @@ import styles from './Card.module.scss';
 
 import Stats from '../Stats/Stats';
 
+import userImage from '../../assets/images/image-victor.jpg';
+
 const Card = () => (
   <React.StrictMode>
-    <div className={styles.wrapper}>
-      <div className={styles.top}></div>
-      <main>
-        <h1></h1>
-        <p></p>
+    <div className={styles.card}>
+      <div className={styles.card__top}></div>
+      <main className={styles.card__main}>
+        <div className={styles.card__imageWrapper}>
+          <img className={styles.card__image} src={userImage} alt="user" />
+        </div>
+        <div className={styles.card__personal}>
+          <h1 className={styles.card__personal_primary}>Victor Crest <span className={styles.card__personal_secondary}>26</span></h1>
+          <p className={styles.card__personal_secondary}>London</p>
+        </div>
+        <Stats />
       </main>
-      <Stats />
-    </div>
-    <div>
-      <img src="" alt="" />
-    </div>
-  </React.StrictMode>
+    </div >
+  </React.StrictMode >
 )
 export default Card;
