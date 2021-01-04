250ee24fa4b6189d05c3814c7cc6db6dc315dfc1
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-05 00:23:57 +0100

    add stats

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -3,7 +3,20 @@ import styles from './Stats.module.scss';
 
 const Stats = () => (
   <React.StrictMode>
-
+    <div className={styles.wrapper}>
+      <div className={styles.stats}>
+        <p className={styles.stats__number}>80K</p>
+        <p className={styles.stats__text}>Followers</p>
+      </div>
+      <div className={styles.stats}>
+        <p className={styles.stats__number}>803K</p>
+        <p className={styles.stats__text}>Likes</p>
+      </div>
+      <div className={styles.stats}>
+        <p className={styles.stats__number}>1.4K</p>
+        <p className={styles.stats__text}>Photos</p>
+      </div>
+    </div>
   </React.StrictMode>
 )
 export default Stats;
