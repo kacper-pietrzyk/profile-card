9104ceb44e057696b17c51a53a2205957d719539
Kacper Pietrzyk <kacper1609@gmail.com>
2021-01-28 23:32:05 +0100

    fetch method to get data from API

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Card.module.scss';
 
 import Stats from '../Stats/Stats';
 
 import userImage from '../../assets/images/image-victor.jpg';
 
-const Card = () => (
-  <>
-    <div className={styles.card}>
-      <div className={styles.card__top}></div>
-      <main className={styles.card__main}>
-        <div className={styles.card__imageWrapper}>
-          <img className={styles.card__image} src={userImage} alt="user" />
-        </div>
-        <div className={styles.card__personal}>
-          <h1 className={styles.card__personal_primary}>Victor Crest <span className={styles.card__personal_secondary}>26</span></h1>
-          <p className={styles.card__personal_secondary}>London</p>
-        </div>
-        <Stats />
-      </main>
-    </div >
-  </>
-)
+const Card = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const url = 'https://next.json-generator.com/api/json/get/EkDkYXnJc';
+    fetch(url)
+      .then(response => response.json())
+      .then(result => setUser(result.results))
+      .catch(err => console.log(`Error: ${err}`));
+  }, [])
+
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.card__top}></div>
+        <main className={styles.card__main}>
+          <div className={styles.card__imageWrapper}>
+            <img className={styles.card__image} src={userImage} alt="user" />
+          </div>
+          <div className={styles.card__personal}>
+            <h1 className={styles.card__personal_primary}>{''} {''} <span className={styles.card__personal_secondary}>26</span></h1>
+            <p className={styles.card__personal_secondary}>London</p>
+          </div>
+          <Stats />
+        </main>
+      </div >
+    </>
+  )
+}
 export default Card;
