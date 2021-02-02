ddb8f02b30fc91858ecd5ef1352bce102086a602
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-03 00:04:39 +0100

    update fetch method to get user's data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Card.module.scss';
 
+// import User from './User';
 import Stats from '../Stats/Stats';
 
 import userImage from '../../assets/images/image-victor.jpg';
 
 const Card = () => {
-  const [user, setUser] = useState();
+  const [users, setUsers] = useState([]);
+  // const [randomUser, setRandomUser] = useState([]);
 
   useEffect(() => {
     const url = 'https://next.json-generator.com/api/json/get/EkDkYXnJc';
     fetch(url)
-      .then(response => response.json())
-      .then(result => setUser(result.results))
+      .then(response => {
+        if (response.status === 200) {
+          return response.json()
+        }
+        throw Error(response.statusText);
+      })
+      .then(result => setUsers(result.results))
       .catch(err => console.log(`Error: ${err}`));
-  }, [])
+    console.log('wywołany useeffect')
+  }, []);
+
+  const index = Math.floor(Math.random() * (users.length + 1));
+  const currentUser = users[index];
+
 
   return (
     <>
@@ -22,12 +34,29 @@ const Card = () => {
         <div className={styles.card__top}></div>
         <main className={styles.card__main}>
           <div className={styles.card__imageWrapper}>
-            <img className={styles.card__image} src={userImage} alt="user" />
+            <img
+              className={styles.card__image}
+              src={userImage}
+              alt="user" />
           </div>
-          <div className={styles.card__personal}>
-            <h1 className={styles.card__personal_primary}>{''} {''} <span className={styles.card__personal_secondary}>26</span></h1>
-            <p className={styles.card__personal_secondary}>London</p>
-          </div>
+          {users.length !== 0 && (
+            <div className={styles.card__personal}>
+              <h1
+                className={styles.card__personal_primary}
+              >
+                {currentUser.name.first} {currentUser.name.last}
+                <span
+                  className={styles.card__personal_secondary}
+                >
+                  {currentUser.age}
+                </span>
+              </h1>
+              <p
+                className={styles.card__personal_secondary}
+              >
+                {currentUser.city}</p>
+            </div>
+          )}
           <Stats />
         </main>
       </div >
