b333bab3530dfbd97949ac032b757480c1837b1f
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-07 19:48:43 +0100

    move user's photo from Card component

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import styles from './User.module.scss';
 
+import Stats from '../Stats/Stats';
+
+import userImage from '../../assets/images/image-victor.jpg';
+
 const User = ({ user }) => {
   return (
-    <div className={styles.card__personal}>
-      <h1
-        className={styles.card__personal_primary}>
-        {user.name.first} {user.name.last}
-        <span
+    <main className={styles.card__main}>
+      <div className={styles.card__imageWrapper}>
+        <img
+          className={styles.card__image}
+          src={userImage}
+          alt="user" />
+      </div>
+      <div className={styles.card__personal}>
+        <h1
+          className={styles.card__personal_primary}>
+          {user.name.first} {user.name.last}
+          <span
+            className={styles.card__personal_secondary}>
+            {user.age}
+          </span>
+        </h1>
+        <p
           className={styles.card__personal_secondary}>
-          {user.age}
-        </span>
-      </h1>
-      <p
-        className={styles.card__personal_secondary}>
-        {user.city}</p>
-    </div>
+          {user.city}</p>
+      </div>
+      <Stats />
+    </main>
   )
 }
 
