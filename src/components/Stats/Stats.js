36edfb7f2b347f53d3e1cd014d939a56a488d2fe
Kacper Pietrzyk <kacper1609@gmail.com>
2021-02-07 20:00:31 +0100

    get stats from user's data

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './Stats.module.scss';
 
-const Stats = () => (
-  <>
-    <div className={styles.wrapper}>
-      <div className={styles.stats}>
-        <p className={styles.stats__number}>80K</p>
-        <p className={styles.stats__text}>Followers</p>
+const Stats = props => {
+  const { followers, likes, photos } = props.user.stats;
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.stats}>
+          <p className={styles.stats__number}>{followers}</p>
+          <p className={styles.stats__text}>Followers</p>
+        </div>
+        <div className={styles.stats}>
+          <p className={styles.stats__number}>{likes}</p>
+          <p className={styles.stats__text}>Likes</p>
+        </div>
+        <div className={styles.stats}>
+          <p className={styles.stats__number}>{photos}</p>
+          <p className={styles.stats__text}>Photos</p>
+        </div>
       </div>
-      <div className={styles.stats}>
-        <p className={styles.stats__number}>803K</p>
-        <p className={styles.stats__text}>Likes</p>
-      </div>
-      <div className={styles.stats}>
-        <p className={styles.stats__number}>1.4K</p>
-        <p className={styles.stats__text}>Photos</p>
-      </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 export default Stats;
