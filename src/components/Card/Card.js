bd82185379849d61085d332b776844585e4c4428
Kacper Pietrzyk <kacper1609@gmail.com>
2021-03-01 19:44:14 +0100

    add button to display other profile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,6 +8,7 @@ import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Card = () => {
   const [users, setUsers] = useState([]);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const url = 'https://next.json-generator.com/api/json/get/EJaOYVFx9';
@@ -22,21 +23,23 @@ const Card = () => {
       .catch(err => console.log(`Error: ${err}`));
   }, []);
 
-  const index = Math.floor(Math.random() * users.length);
+  const drawIndex = () => {
+    const newIndex = Math.floor(Math.random() * users.length);
+    setIndex(newIndex);
+  }
+
   const currentUser = users[index];
 
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.card__top}>
-          <FontAwesomeIcon icon={faSyncAlt} className={styles.card__button} />
-        </div>
-        {users.length !== 0 &&
-          <User
-            user={currentUser}
-          />}
-      </div >
-    </>
+    <div className={styles.card}>
+      <div className={styles.card__top}>
+        <FontAwesomeIcon icon={faSyncAlt} className={styles.card__button} onClick={drawIndex} />
+      </div>
+      {users.length !== 0 &&
+        <User
+          user={currentUser}
+        />}
+    </div >
   )
 }
 export default Card;
